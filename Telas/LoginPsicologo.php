ba84648ceca97899104481aa6cb3c7c858db5447
VINNYSCOSTA <Vinicius00.vc@gmail.com>
2021-10-19 22:59:12 -0300

    login psicologo com gets

--- Telas/LoginPsicologo.php
+++ Telas/LoginPsicologo.php
@@ -1,19 +1,22 @@
-<?php
-   ob_start();
-   session_start();
-?>
-
-<?
-   // error_reporting(E_ALL);
-   // ini_set("display_errors", 1);
-?>
-
+<!DOCTYPE html>
 <html lang = "en">
    
     <head>
       <title>Login</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-      
+      <script>
+         function Authentication(){
+            var email = document.getElementById('textEmail').value;
+            var password = document.getElementById('textSenha').value;
+            var crp = document.getElementById('textCRP').value;
+            var auth = false; /*Função retorna verdadeiro ou falso*/
+            if (auth === true){
+               window.location.href = 'AlterarCliente.php';    //tela de exemplo para saber se funciona, a tela inicial do psicologo sera Gerenciamento ou a tela com as informações dele.
+            }else{
+               alert("E-mail ou Senha incorretos. Tente Novamente!");
+            }
+         }
+      </script>
       <style>
          body {
             padding-top: 40px;
@@ -83,30 +86,28 @@
 
   
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
                         
-        <form class = "form-signin" role = "form" 
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
+      <div class="container form-signin ">
             
             <h1>Entrar</h1>
             
             <input type = "text" class = "form-control" 
-               name = "email" placeholder = "Email" required ></br>
+               name = "email" id="textEmail" placeholder = "Email" required ></br>
             
-           <input type = "password" class = "form-control"
-               name = "senha" placeholder = "Senha" required>
+            <input type = "password" class = "form-control"
+               name = "senha" id="textSenha" placeholder = "Senha" required>
 
             <input type = "text" class = "form-control" 
-               name = "crp" placeholder = "CRP" required ></br>
+               name = "crp" id="textCRP" placeholder = "CRP" required ></br>
 
             <h2>
-           <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "entrar">
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" name = "entrar">
                 Entrar
-           </button>
+            </button>
             <h2>
             <a href = "CadastroPsicologo.php">Não possui uma conta? Cadastre-se
-         </form>                  
+      </div>                  
       
    </body>
 </html>
